baf65f559d1aa8451d96166d75908c81ad4e0814
Sago0 <sergey.gapeev@okay.qa>
2022-08-10 13:58:00 +0300

    update for YandexTests

--- src/test/java/YandexTestBase.java
+++ src/test/java/YandexTestBase.java
@@ -1,15 +1,15 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
-
-import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
 public class YandexTestBase {
 
     @BeforeAll
     static void setup() {
         Configuration.browserSize = "2560x1440";
-        addListener("AllureSelenide", new AllureSelenide());
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "https://yandex.by/";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
